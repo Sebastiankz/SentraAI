a30a9145373e04d69394983ec85bd4750a87a8d1
sebastiankz <sebastianolivo@uninorte.edu.co>
2026-06-28 17:20:51 -0500

    feat: Validations were added to verify the HMAC signature from GitHub

--- app/main.py
+++ app/main.py
@@ -1,7 +1,43 @@
-from fastapi import FastAPI
+import os
+from dotenv import load_dotenv
+from fastapi import FastAPI, Request, HTTPException
+import hmac
+import hashlib
+
+load_dotenv()  # Carga las variables de entorno desde el archivo .env
+print("CWD:", os.getcwd())
+
+GITHUB_WEBHOOK_SECRET = os.getenv("GITHUB_WEBHOOK_SECRET")
 
 app = FastAPI() #instancia de la app (no el server, el server es uvicorn)
 
+def verify_signature(raw_body: bytes, signature_header: str, secret: str) -> bool:
+    # Calculamos NUESTRA firma sobre el cuerpo crudo, con el secreto.
+    expected = "sha256=" + hmac.new(
+        key=secret.encode(),          # el secreto debe ir en bytes
+        msg=raw_body,                 # el cuerpo CRUDO (¡por esto lo guardamos intacto!)
+        digestmod=hashlib.sha256,     # el algoritmo: SHA-256
+    ).hexdigest()
+
+    return hmac.compare_digest(expected, signature_header)
+
 @app.get("/health")
-async def health():
+def health():
     return {"status": "ok"}
+
+@app.post("/webhooks/github")
+async def github_webhook(request: Request):
+    raw_body = await request.body()
+
+    signature_header = request.headers.get("X-Hub-Signature-256")
+    if not signature_header:
+        raise HTTPException(status_code=400, detail="Missing signature header")
+    
+    if not verify_signature(raw_body, signature_header, GITHUB_WEBHOOK_SECRET):
+        raise HTTPException(status_code=401, detail="Invalid signature")
+
+    event_type = request.headers.get("X-GitHub-Event")
+    print(f"Webhook received: {event_type}, {len(raw_body)} bytes")
+
+    return {"status:": "received"}
+
